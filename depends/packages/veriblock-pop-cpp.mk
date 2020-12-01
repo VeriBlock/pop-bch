6a509b8132182994bafe3835aa5594f517b9b668
Bohdan Vanieiev <warchantua@gmail.com>
2020-12-01 19:07:23 +0200

    Add CI/CD

---
+++ depends/packages/veriblock-pop-cpp.mk
@@ -0,0 +1,46 @@
+package=veriblock-pop-cpp
+$(package)_version=92eb28f67e65585b6ca02e45b2a1215e70de9d2c
+$(package)_download_path=https://github.com/VeriBlock/alt-integration-cpp/archive/
+$(package)_file_name=$($(package)_version).tar.gz
+$(package)_sha256_hash=c83af1abd331a2ac19289f0f16b201460f87303a8be3efdaf3940c97b5d3eb52
+$(package)_build_subdir=build
+$(package)_build_type=$(BUILD_TYPE)
+$(package)_asan=$(ASAN)
+
+define $(package)_preprocess_cmds
+  mkdir -p build
+endef
+
+ifeq ($(strip $(HOST)),)
+  define $(package)_config_cmds
+    cmake -DCMAKE_INSTALL_PREFIX=$(host_prefix) -DCMAKE_BUILD_TYPE=$(package)_build_type \
+    -DTESTING=OFF -DSHARED=OFF -DASAN:BOOL=$(package)_asan ..
+  endef
+else ifeq ($(HOST), x86_64-apple-darwin16)
+  define $(package)_config_cmds
+    cmake -DCMAKE_C_COMPILER=$(darwin_CC) -DCMAKE_CXX_COMPILER=$(darwin_CXX) \
+    -DCMAKE_INSTALL_PREFIX=$(host_prefix) -DCMAKE_BUILD_TYPE=$(package)_build_type \
+    -DCMAKE_OSX_ARCHITECTURES=x86_64 -DCMAKE_SYSTEM_NAME=Darwin -DCMAKE_SYSTEM_PROCESSOR=x86_64 \
+    -DCMAKE_C_COMPILER_TARGET=$(HOST) -DCMAKE_CXX_COMPILER_TARGET=$(HOST) \
+    -DCMAKE_OSX_SYSROOT=$(OSX_SDK) -DTESTING=OFF \
+    -DSHARED=OFF ..
+  endef
+else ifeq ($(HOST), x86_64-pc-linux-gnu)
+  define $(package)_config_cmds
+    cmake -DCMAKE_INSTALL_PREFIX=$(host_prefix) -DCMAKE_BUILD_TYPE=$(package)_build_type \
+    -DTESTING=OFF -DSHARED=OFF ..
+  endef
+else
+  define $(package)_config_cmds
+    cmake -DCMAKE_C_COMPILER=$(HOST)-gcc -DCMAKE_CXX_COMPILER=$(HOST)-g++ \
+    -DCMAKE_INSTALL_PREFIX=$(host_prefix) -DTESTING=OFF -DSHARED=OFF ..
+  endef
+endif
+
+define $(package)_build_cmds
+  $(MAKE)
+endef
+
+define $(package)_stage_cmds
+  $(MAKE) DESTDIR=$($(package)_staging_dir) install
+endef
