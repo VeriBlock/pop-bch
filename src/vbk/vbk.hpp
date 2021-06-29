bitcoin/bitcoin

Bfistatus (#499)

src/vbk/vbk.hpp
@@ -13,6 +13,7 @@ namespace VeriBlock {
 using KeystoneArray = std::array<uint256, 2>;
 
 const static int32_t POP_BLOCK_VERSION_BIT = 0x80000UL;
+const static int64_t ALT_CHAIN_ID = 0x3ae6ca;
 
 }  // namespace VeriBlock
 

VeriBlock/pop-bch

(cherry picked from commit 0e2b63783afdc1d6795506e3beb8dad9e73fad49)

src/vbk/vbk.hpp
@@ -13,6 +13,7 @@ namespace VeriBlock {
 using KeystoneArray = std::array<uint256, 2>;
 
 const static int32_t POP_BLOCK_VERSION_BIT = 0x80000UL;
+const static int64_t ALT_CHAIN_ID = 0x3e4fac;
 
 }  // namespace VeriBlock
 